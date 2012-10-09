7919a082016d7ead75b77e8a40369b28448c236e
noisette.guillaume@gmail.com <noisette.guillaume@gmail.com@f5b83611-3ae7-c801-ab72-dcc51d5799a7>
2012-10-09 16:29:03 +0000

    [BallFiction] : Synchronisation

--- trunk/Assets/Script/BreadmillScript.cs
+++ trunk/Assets/Script/BreadmillScript.cs
@@ -17,18 +17,22 @@ public class BreadmillScript : MonoBehaviour {
 	
 	// Collision pour faire bouger la bille
 	void OnCollisionStay(Collision collision) {
-		if(_ballDirection == Direction.Forward) {
-			collision.gameObject.rigidbody.AddForce(Vector3.forward * _ballForce);
-		} else if(_ballDirection == Direction.Back) {
-			collision.gameObject.rigidbody.AddForce(Vector3.back * _ballForce);
-		} else if(_ballDirection == Direction.Left) {
-			collision.gameObject.rigidbody.AddForce(Vector3.left * _ballForce);
-		} else if(_ballDirection == Direction.Right) {
-			collision.gameObject.rigidbody.AddForce(Vector3.right * _ballForce);
-		} else if(_ballDirection == Direction.Up) {
-			collision.gameObject.rigidbody.AddForce(Vector3.up * _ballForce);
-		} else if(_ballDirection == Direction.Down) {
-			collision.gameObject.rigidbody.AddForce(Vector3.down * _ballForce);
-		}
+		//Debug.Log(collision.gameObject.rigidbody.velocity);
+		BallScript ball = collision.gameObject.GetComponent<BallScript>();
+		//if(collision.gameObject.rigidbody.velocity < ball._absolueVectorForceBall) {
+			if(_ballDirection == Direction.Forward) {
+				collision.gameObject.rigidbody.AddForce(Vector3.forward * _ballForce);
+			} else if(_ballDirection == Direction.Back) {
+				collision.gameObject.rigidbody.AddForce(Vector3.back * _ballForce);
+			} else if(_ballDirection == Direction.Left) {
+				collision.gameObject.rigidbody.AddForce(Vector3.left * _ballForce);
+			} else if(_ballDirection == Direction.Right) {
+				collision.gameObject.rigidbody.AddForce(Vector3.right * _ballForce);
+			} else if(_ballDirection == Direction.Up) {
+				collision.gameObject.rigidbody.AddForce(Vector3.up * _ballForce);
+			} else if(_ballDirection == Direction.Down) {
+				collision.gameObject.rigidbody.AddForce(Vector3.down * _ballForce);
+			}
+		//}
 	}
 }
