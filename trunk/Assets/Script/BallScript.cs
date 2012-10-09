9f28f532622bfcb53a271c8d06507f8d624de6fa
larbi.djedoui <larbi.djedoui@f5b83611-3ae7-c801-ab72-dcc51d5799a7>
2012-10-09 19:31:36 +0000

    [BallFiction] - Ajout des commentaires dans BallScript et TrayScript

--- trunk/Assets/Script/BallScript.cs
+++ trunk/Assets/Script/BallScript.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Script de comportement de la bille
+/// </summary>
 public class BallScript : MonoBehaviour {
 	public GameObject mainCamera;
 	
@@ -9,15 +12,24 @@ public class BallScript : MonoBehaviour {
 	/// </summary>
     public Vector3 _absolueVectorForceBall = new Vector3(2,2,2);
 	
+	/// <summary>
+	/// La hauteur de la caméra par rapport à la bille
+	/// </summary>
+	public float _hauteurCamera = 4f;
+	
+	/// <summary>
+	/// Le recul de la caméra par rapport à la bille
+	/// </summary>
+	public float _reculCamera = 3f;
+	
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		this.mainCamera.transform.position = new Vector3(this.gameObject.transform.position.x,
-														 this.gameObject.transform.position.y + 4f,
-														 this.gameObject.transform.position.z - 3f);
+														 this.gameObject.transform.position.y + this._hauteurCamera,
+														 this.gameObject.transform.position.z - this._reculCamera);
 	}
 }
--- trunk/Assets/Script/TrayScript.cs
+++ trunk/Assets/Script/TrayScript.cs
@@ -1,29 +1,60 @@
 using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Script gérant le comportement de basculement du plateau
+/// </summary>
 public class TrayScript : MonoBehaviour {
+	/// <summary>
+	/// La vitesse à laquelle la rotation s'effectue
+	/// </summary>
     public float _rotationSpeedTray = 0.3f;
+	/// <summary>
+	/// La force de répulsion du plateau (vers le haut)
+	/// </summary>
     public float _repulsionForce = 0.01f;
+	
+	/// <summary>
+	/// La limite de droite
+	/// </summary>
+	public float _maximumRight = -0.15f;
+	/// <summary>
+	/// La limite de gauche
+	/// </summary>
+	public float _maximumLeft = 0.15f;
 	
     // Définition du type d'évènement
     public delegate void ActionEvent();
-
+	
+	/// <summary>
+	/// Event appelé quand on bascule le plateau vers la gauche
+	/// </summary>
     static public event ActionEvent OnLeft;
+	/// <summary>
+	/// Event appelé quand on bascule le plateau vers la droite
+	/// </summary>
     static public event ActionEvent OnRight;
 	
-	// Evènement déclanché lors de l'appui sur le bouton gauche
-    void OnLeftEventReceived()
-    {
-        this.transform.RotateAround(Vector3.forward, _rotationSpeedTray * Time.deltaTime);
+	/// <summary>
+	/// Evènement déclenché lors de l'appui sur le bouton qui doit faire basculer le plateau vers la gauche
+	/// </summary>
+    void OnLeftEventReceived() {
+		if(this.transform.rotation.z < this._maximumLeft) {
+        	this.transform.RotateAround(Vector3.forward, this._rotationSpeedTray * Time.deltaTime);
+		}
     }
-	// Evènement déclanché lors de l'appui sur le bouton droite
-    void OnRightEventReceived()
-    {
-        this.transform.RotateAround(Vector3.back, _rotationSpeedTray * Time.deltaTime);
+	/// <summary>
+	/// Evènement déclenché lors de l'appui sur le bouton qui doit faire basculer le plateau vers la droite
+	/// </summary>
+    void OnRightEventReceived() {
+		if(this.transform.rotation.z > this._maximumRight) {
+	        this.transform.RotateAround(Vector3.back, this._rotationSpeedTray * Time.deltaTime);
+		}
     }
 	
 	// Use this for initialization
 	void Start () {
+		//Ajout des évènements lors du basculement vers la gauche ou la droite
         OnLeft += new ActionEvent(OnLeftEventReceived);
         OnRight += new ActionEvent(OnRightEventReceived);
 	}
