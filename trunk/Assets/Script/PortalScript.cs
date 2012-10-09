93f64fc3d2a19164339e89e621e52e249d19b5b0
noisette.guillaume@gmail.com <noisette.guillaume@gmail.com@f5b83611-3ae7-c801-ab72-dcc51d5799a7>
2012-10-09 20:50:39 +0000

    [BallFiction] : Ajout des portails

---
+++ trunk/Assets/Script/PortalScript.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Script de comportement d'un portail
+/// </summary>
+public class PortalScript : ElementScript {
+	/// <summary>
+	/// L 'autre extremité du téléport
+	/// </summary>
+	public GameObject _otherEnd;
+	/// <summary>
+	/// Delta vector afin d'éviter d'apparaître sous l'objet
+	/// </summary>
+	public Vector3 _teleportDelta = new Vector3(0.01f, 0.01f, 0.01f);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		base.UpdateState();
+	}
+	
+	// Collision pour faire bouger la bille
+	void OnCollisionEnter(Collision collision) {
+		if(this._isButtonActivated) {
+			// Changement de la position de la bille avec le delta
+			collision.gameObject.transform.position = this._otherEnd.gameObject.transform.position + this._teleportDelta;
+			
+			// Vérification si l'autre extremité est un portail
+			if(this._otherEnd.GetComponent<PortalScript>() != null) {
+				// Permet d'éviter les téléports infinis
+				this._otherEnd.GetComponent<PortalScript>()._isButtonActivated = false;
+			}
+		}
+	}
+	
+	// Collision pour modifier l'état du portail
+	void OnCollisionExit(Collision collision) {
+		this._isButtonActivated = true;
+	}
+}
