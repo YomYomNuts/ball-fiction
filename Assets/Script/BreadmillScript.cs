9a0a9250d7b2be8730c94b53655db3fe80f4e178
noisette.guillaume <noisette.guillaume@gmail.com>
2012-10-10 20:28:57 +0000

    [BallFiction] : Ajout de material pour les tapis roulants back et forward + ajout d'un booléen pour ignorer la vitesse maximale de la bille

--- Assets/Script/BreadmillScript.cs
+++ Assets/Script/BreadmillScript.cs
@@ -9,11 +9,14 @@ public class BreadmillScript : MonoBehaviour {
 	/// La force avec laquelle les tapis roulants agissent sur la bille
 	/// </summary>
     public float _ballForce = 0.5f;
-	
 	/// <summary>
 	/// Direction dans laquelle le tapis roulants "pousse" la bille
 	/// </summary>
     public Utils.Direction _ballDirection = Utils.Direction.Forward;
+	/// <summary>
+	/// Prise en compte ou non de la force maximale de la bille
+	/// </summary>
+    public bool _IgnoreMaxiamalForceBall = false;
 
 	// Use this for initialization
 	void Start () {
@@ -54,10 +57,11 @@ public class BreadmillScript : MonoBehaviour {
 		BallScript ball = collision.gameObject.GetComponent<BallScript>();
 		// Variable locale contenant la velocité de la bille
 		Vector3 velocity = collision.gameObject.rigidbody.velocity;
-		// On vérifie que la bille n'a pas encore atteint sa vitesse maximale
-		if(Mathf.Abs(velocity.x) < Mathf.Abs(ball._absolueVectorForceBall.x) && 
+		// On vérifie que la bille n'a pas encore atteint sa vitesse maximale ou que l'on ignore sa vitesse maximale
+		if(this._IgnoreMaxiamalForceBall ||
+			(Mathf.Abs(velocity.x) < Mathf.Abs(ball._absolueVectorForceBall.x) && 
 			Mathf.Abs(velocity.y) < Mathf.Abs(ball._absolueVectorForceBall.y) && 
-			Mathf.Abs(velocity.z) < Mathf.Abs(ball._absolueVectorForceBall.z)) {
+			Mathf.Abs(velocity.z) < Mathf.Abs(ball._absolueVectorForceBall.z))) {
 			// On applique la force selon la direction
 			if(this._ballDirection == Utils.Direction.Forward) {
 				collision.gameObject.rigidbody.AddForce(Vector3.forward * this._ballForce);
